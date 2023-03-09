fd757271893bee8cb0effd7a60f684aa4925f765
Aminul <mdaminulislamsahin14@gmail.com>
2023-03-09 18:05:33 +0600

    restaurant mobile menus done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
-function showNav () {
+function showNav (data) {
+
+    const searchBar = document.getElementById("search");
+    const restaurantFoodMenu = document.getElementById("restaurant-food-menu");
+    const restaurantFoodMenuMb = document.getElementById("restaurant-mb-menu");
+  
+    if(data === "open") {
+        searchBar.classList.add("d-none")
+        restaurantFoodMenu.classList.add("d-none")
+        restaurantFoodMenuMb.classList.add("d-none")
+    }
+    if(data === "close") {
+        searchBar.classList.remove("d-none")
+        restaurantFoodMenu.classList.remove("d-none")
+        restaurantFoodMenuMb.classList.remove("d-none")
+    }
+
     document.getElementsByClassName("mobile-menu-wrapper")[0].classList.toggle("d-none")
 }
